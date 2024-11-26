99fe9796df88e9a3cf0b7fbe0750a1e1cbc1a300
Giselle Lopes <giselle.lopes@poatek.com>
2024-11-26 13:43:02 -0300

    first commit

---
+++ src/app/components/moment-form/moment-form.component.ts
@@ -0,0 +1,51 @@
+import { NgIf } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Moment } from '../../Moment';
+
+@Component({
+  selector: 'app-moment-form',
+  standalone: true,
+  imports: [FormsModule, ReactiveFormsModule, NgIf],
+  templateUrl: './moment-form.component.html',
+  styleUrl: './moment-form.component.css'
+})
+export class MomentFormComponent {
+  @Output() onSubmit = new EventEmitter<Moment>()
+  @Input() btnText!: string;
+
+  momentForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl('')
+    })
+  }
+
+  get title() {
+    return this.momentForm.get('title')!;
+  }
+
+  get description() {
+    return this.momentForm.get('description')!;
+  }
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0]
+
+    this.momentForm.patchValue({ image: file })
+  }
+
+  submit() {
+    if (this.momentForm.invalid) {
+      return;
+    }
+    
+    console.log('Enviou formulário')
+
+    this.onSubmit.emit(this.momentForm.value);
+  }
+}
